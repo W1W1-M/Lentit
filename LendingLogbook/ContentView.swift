3b2b5b81ee289d48768a71c02a34094f8899471f
W1W1-M <w1w1_m@icloud.com>
2021-12-19 21:10:22 +0100

    Initial Commit

---
+++ LendingLogbook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LendingLogbook
+//
+//  Created by William Mead on 19/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LendingLogbook/LendingLogbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LendingLogbookApp.swift
+//  LendingLogbook
+//
+//  Created by William Mead on 19/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct LendingLogbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
